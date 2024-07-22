14295fbf792822118b5d690a8cff8621bfef227b
suleman111111111111111@gmail.com <suleman111111111111111@gmail.com>
2024-07-22 10:11:35 +0530

    nav done

--- eulersMerhod.js
+++ eulersMerhod.js
@@ -73,3 +73,21 @@ document.getElementById('euler-form').addEventListener('submit', function(event)
         polynomialsElement.innerHTML += `Step ${index+1}: ${poly}\n`;
     });
 });
+
+
+
+let menuToggle = document.querySelector('.menuToggle');
+let sidebar = document.querySelector('.sidebar');
+menuToggle.onclick =function(){
+menuToggle.classList.toggle('active');
+sidebar.classList.toggle('active');}
+
+let Menulist= document.querySelectorAll('.Menulist li');
+function activeLink(){
+    Menulist.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active')
+}
+
+Menulist.forEach((item) =>
+item.addEventListener('click', activeLink));
